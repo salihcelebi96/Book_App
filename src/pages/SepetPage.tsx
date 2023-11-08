fa535fd9abb62f631641cb8a00b0f5d51c927a63
salihcelebi96 <celebisalih277@gmail.com>
2023-11-08 04:36:07 +0300

    8.commit

--- src/pages/SepetPage.tsx
+++ src/pages/SepetPage.tsx
@@ -45,30 +45,35 @@ const SepetPage: React.FC = () => {
     <div>
       <div className='relative'>
         <div className='grid md:grid-cols-2 mx-10 sm:grid-cols-1 lg:grid-cols-4'>
-          {basket.map((book, index) => (
-            <div className='p-5 h-[600px] border relative m-4' key={index}>
-              <h1 className="text-lg">{book.volumeInfo.title}</h1>
-              <img
-                id='img-book'
-                className='w-full h-[400px] py-4 hover:scale-110 transition-transform duration-500'
-                src={book.volumeInfo.imageLinks?.thumbnail || 'fallback-image-url'}
-                alt="Book Cover"
-              />
-              <p>Author: {book.volumeInfo.authors.join(', ')}</p>
-              <p>Page Count: {book.volumeInfo.pageCount}</p>
-              <p>Price: {book.saleInfo?.listPrice?.amount || 'N/A'} TL</p>
-              <div className='absolute left-0 flex  h-10 hover:bg-orange-500 bottom-0 w-full cursor-pointer bg-orange-600'>
-                <Link className='text-white w-1/2 bg-green-600 hover:bg-green-500 text-xl flex items-center justify-center' to="/payment">
-                  Satın Al
-                </Link>
-                <div onClick={() => {dispatch(deleteBasket(book.id))}} className='text-white w-1/2 h-full bg-red-600 hover-bg-red-400 cursor-pointer text-xl flex items-center justify-center'>
-                  <p>Kaldır</p>
-                </div>
-
-
-              </div>
+          {basket.length === 0 ? (
+            <div className=' w-screen flex justify-center   text-3xl items-center h-screen '>
+              <p>Sepette Ürün Bulunmamaktadır</p>
             </div>
-          ))}
+            
+          ) : (
+            basket.map((book, index) => (
+              <div className='p-5 h-[600px] border relative m-4' key={index}>
+                <h1 className="text-lg">{book.volumeInfo.title}</h1>
+                <img
+                  id='img-book'
+                  className='w-full h-[400px] py-4 hover:scale-110 transition-transform duration-500'
+                  src={book.volumeInfo.imageLinks?.thumbnail || 'fallback-image-url'}
+                  alt="Book Cover"
+                />
+                <p>Author: {book.volumeInfo.authors.join(', ')}</p>
+                <p>Page Count: {book.volumeInfo.pageCount}</p>
+                <p>Price: {book.saleInfo?.listPrice?.amount || 'N/A'} TL</p>
+                <div className='absolute left-0 flex h-10 hover:bg-orange-500 bottom-0 w-full cursor-pointer bg-orange-600'>
+                  <Link className='text-white w-1/2 bg-green-600 hover:bg-green-500 text-xl flex items-center justify-center' to="/payment">
+                    Satın Al
+                  </Link>
+                  <div onClick={() => {dispatch(deleteBasket(book.id))}} className='text-white w-1/2 h-full bg-red-600 hover-bg-red-400 cursor-pointer text-xl flex items-center justify-center'>
+                    <p>Kaldır</p>
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </div>
