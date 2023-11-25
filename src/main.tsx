66c8cc90c963a9655fe841cf602554f3aa78ed32
salihcelebi96 <celebisalih277@gmail.com>
2023-11-26 00:09:28 +0300

    15.commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 
 import ReactDOM from 'react-dom'; // react-dom/client yerine react-dom kullanın
-import App from './App.jsx'; // .jsx uzantısını kullanın
+import App from './App'; // .jsx uzantısını kullanın
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    
+      <App />
+    
   </Provider>,
   document.getElementById('root')
 );
